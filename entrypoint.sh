5c805353f05d1d9a647580426a454a523eb092b7
mrachuta <34251394+mrachuta@users.noreply.github.com>
2026-04-05 00:32:05 +0200

    Update entrypoint to handle debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ input_token=$1
 input_repository=$2
 input_pull_request_id=$3
 input_mode=$4
-debug=''
+debug=""
 
 if [[ "$ACTIONS_STEP_DEBUG" == "true" ]]; then
-  debug='--debug'
+  debug="--debug"
 fi
 
-python3 roprl.py --token "$input_token" --repository "$input_repository" --pull-request-id "$input_pull_request_id" --mode "$input_mode" "$debug"
+python3 roprl.py --token "$input_token" --repository "$input_repository" --pull-request-id "$input_pull_request_id" --mode "$input_mode" $debug
