f500716e399d581b4337a87936a111474d371ec6
mrachuta <34251394+mrachuta@users.noreply.github.com>
2026-04-05 01:12:51 +0200

    Simpler approach for debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ input_token=$1
 input_repository=$2
 input_pull_request_id=$3
 input_mode=$4
-debug=$5
+debug=""
 
-python3 roprl.py --token "$input_token" --repository "$input_repository" --pull-request-id "$input_pull_request_id" --mode "$input_mode" "$debug"
+if [[ "$5" == "true" || "$5" == "TRUE" || "$5" == "True" || "$5" == "1" ]]; then
+  debug="--debug"
+fi
+
+python3 roprl.py --token "$input_token" --repository "$input_repository" --pull-request-id "$input_pull_request_id" --mode "$input_mode" $debug
